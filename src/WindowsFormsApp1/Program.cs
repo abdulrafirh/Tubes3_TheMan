6e91efb30625d552b281171e2168640727e60a2f
abdulrafirh <abdulrafi623@gmail.com>
2024-06-08 12:51:37 +0700

    feat: DB gaming

--- src/WindowsFormsApp1/Program.cs
+++ src/WindowsFormsApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
         }
     }
 }
---
+++ src/bahasaAlay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public sealed class AlayDetector
+{
+    private AlayDetector() { }
+
+    private static readonly AlayDetector instance = new AlayDetector();
+
+    public static AlayDetector Instance
+    {
+        get { return instance; }
+    }
+
+    public bool IsAlay(string firstString, string secondString)
+    {
+        if (string.IsNullOrEmpty(firstString) || string.IsNullOrEmpty(secondString))
+        {
+            return false;
+        }
+
+        firstString = firstString.ToLower();
+        secondString = secondString.ToLower();
+
+        if (firstString.Length != secondString.Length)
+        {
+            return false;
+        }
+
+        int j = 0;
+
+        for (int i = 0; i < firstString.Length; i++)
+        {
+            char c = firstString[i];
+
+            if (IsVowel(c))
+            {
+                continue;
+            }
+
+            if (j >= secondString.Length || secondString[j] != c)
+            {
+                return false;
+            }
+
+            j++;=
+        }
+
+        return true;
+    }
+
+    private bool IsVowel(char c)
+    {
+        return "aeiouAEIOU".IndexOf(c) >= 0;
+    }
+}
